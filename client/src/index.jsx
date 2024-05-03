c6ce183555c30e218fa46b1c00379cc34de13c1e
swapnil-sriv <swapnilsrivastava2002@gmail.com>
2024-05-04 01:18:52 +0530

    v2.1.0 updated codebase to React 18

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { useState } from "react";
+import {createRoot} from "react-dom/client";
+
+const root = document.getElementById('root');
 
 
-
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
+createRoot(root,{concurrent:true}).render(<App/>);
 
